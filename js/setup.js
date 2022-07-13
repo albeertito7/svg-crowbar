f36e6d0c94fcb6b29ca918a6c747973725554815
albert <albertperezdatsira@gmail.com>
2022-07-13 17:44:53 +0200

    Create setup.js

---
+++ js/setup.js
@@ -0,0 +1,6 @@
+function setup() {
+    let e = document.createElement('script');
+    e.setAttribute('src', 'https://albeertito7.github.io/svg-crowbar/svg-crowbar.js'); 
+    e.setAttribute('class', 'svg-crowbar');
+    document.body.appendChild(e);
+}
